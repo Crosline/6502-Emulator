d1b3d18f25bc1bf2b5f0a080ec5fafe0ec99d3e9
Çağkan Çağlayanel <cagkancaglayanel@gmail.com>
2022-10-06 20:27:40 +0300

    Fix: Accumulator cycle pass for zero page

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,14 @@ int main() {
 
 	//mem[0xFFFC] = CPU::LDA_IMM;
 	//mem[0xFFFD] = 0x42;
+	
+	//cpu.LoadAccumulator(mem, 2);
 
 	mem[0xFFFC] = CPU::LDA_ZP;
 	mem[0xFFFD] = 0x42;
 	mem[0x0042] = 0x84;
 
-	cpu.LoadAccumulator(mem, 2);
+	cpu.LoadAccumulator(mem, 3);
 
 	return 0;
 }
